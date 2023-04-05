0c120a657a48733de2b1be5731b191cf84246657
Cristian F. Patiño Rojas <e.swiosdev2.cdo@telefonica.com>
2023-04-05 20:53:23 +0200

    Add dot files visibility methods

--- modules/misc.sh
+++ modules/misc.sh
@@ -1,2 +1,12 @@
+showDotfiles() {
+	defaults write com.apple.Finder AppleShowAllFiles true;
+	killall Finder	
+}
+
+hideDotfiles() {
+	defaults write com.apple.Finder AppleShowAllFiles false;
+	killall Finder	
+}
+
 decodeProvision() { security cms -D -i  $1 }
 runEmulator() { ~/Library/Android/sdk/tools/emulator -avd Pixel_2_API_29 -no-snapshot }
